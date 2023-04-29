1df5246892c9b10237e3f42072e71653bff41ed7
Akshay Moolya <akshaymoolya88@gmail.com>
2023-04-29 19:08:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./component/Header/Header";
+import Home from "./pages/Home/Home";
+import CoinPage from "./pages/CoinPage/CoinPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" Component={Home} exact />
+            <Route path="/coins/:id" Component={CoinPage} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
